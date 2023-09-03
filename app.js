f77dceac4b222d2e49afbcec4a7185eefe5a0ff2
youKnow9 <l.pelevina.che@gmail.com>
2023-09-03 12:35:24 +0300

    add

--- app.js
+++ app.js
@@ -5,38 +5,37 @@ class CountdownTimer extends HTMLElement {
 		this.endTime = null;
 		this.time = false;
 		this.int = null;
+		this.started = false;
 	}
       
 	connectedCallback() {
 		this.render();
-		this.initializeEventListeners();
+		this.test()
 	}
       
-	initializeEventListeners() {
-		this.addEventListener('startTimer', () => this.startCountDown());
-		this.addEventListener('pauseTimer', () => this.pauseCountDown());
-		this.addEventListener('resetTimer', () => this.resetCountDown());
-	}
+	test() {
+		const startButton = document.getElementById('startButton');
+		startButton.addEventListener('click', (e) => {
+			console.log(e.target)
+			if (!this.started) {
+				this.started = true;
+				this.startCountDown();
+			}
+		});
+		const pauseButton = document.getElementById('pauseButton');
+		const resetButton = document.getElementById('resetButton');
+		pauseButton.addEventListener('click', () => this.pauseCountDown());
+		resetButton.addEventListener('click', () => this.resetCountDown());
+	};
       
 	static get observedAttributes() {
 	  	return ['seconds', 'to-time'];
-	}
+	};
       
 	attributeChangedCallback(name, oldValue, newValue) {
-		if (name === 'seconds') {
-			const seconds = parseInt(newValue);
-			this.endTime = Date.now() + seconds * 1000;
-		} 
-		// else if (name === 'to-time') {
-		// 	const [hours, minutes, seconds] = newValue.split(':').map(Number);
-		// 	const targetTime = new Date();
-		// 	targetTime.setHours(hours);
-		// 	targetTime.setMinutes(minutes);
-		// 	targetTime.setSeconds(seconds);
-		
-		// 	this.endTime = targetTime.getTime();
-		// };
-
+		const seconds = parseInt(newValue);
+		this.endTime = Date.now() + seconds * 1000; // вычисляем время завершения таймера 
+		console.log(this.endTime)
 		if (this.time) {
 			this.updateTimeDisplay();
 		};
@@ -52,7 +51,7 @@ class CountdownTimer extends HTMLElement {
 		</style>
 		<span id="timer">00:00:00</span>
 		`;
-	}
+	};
       
 	updateTimeDisplay() {
 		const timerElement = this.shadowRoot.getElementById('timer');
@@ -64,69 +63,70 @@ class CountdownTimer extends HTMLElement {
 			const hours = Math.floor(timeDifference / 3600000);
 			const minutes = Math.floor((timeDifference % 3600000) / 60000);
 			const seconds = Math.floor((timeDifference % 60000) / 1000);
-		
+
 			const formattedTime = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
 	
-			timerElement.innerText = formattedTime;
-		}
-	}
+			timerElement.innerText = `${formattedTime}`;
+		};
+	};
       
 	startCountDown() {
 		this.time = true;
 		if (this.time) {
-			
 			this.int = setInterval(() => {
 				this.updateTimeDisplay();
 				if (this.endTime - Date.now() <= 0) {
 					clearInterval(this.int);
-					this.dispatchEvent(new Event('endtimer'));
-				}
+					// this.dispatchEvent(new Event('endtimer'));
+				};
 			}, 1000);
-		}
-	}
+		};
+	};
       
 	pauseCountDown() {
 	  	clearInterval(this.int);
-	}
+	};
       
 	resetCountDown() {
 		clearInterval(this.int);
 		this.time = false;
 		this.render();
-	}
+	};
 }
 customElements.define('countdown-timer', CountdownTimer);
-const startButton = document.createElement('button');
-const pauseButton = document.createElement('button');
-const resetButton = document.createElement('button');
-// Блок с созданием и добавлением кнопок в DOM
-window.addEventListener('DOMContentLoaded', () => {
-	const startButton = document.createElement('button');
-	startButton.textContent = 'Start Timer';
 
-	startButton.addEventListener('click', () => {
-		const timerElement = document.querySelector('countdown-timer');
-		timerElement.dispatchEvent(new CustomEvent('startTimer'));
-	});
 
-	const pauseButton = document.createElement('button');
-	pauseButton.textContent = 'Pause Timer';
+// const startButton = document.createElement('button');
+// const pauseButton = document.createElement('button');
+// const resetButton = document.createElement('button');
 
-	pauseButton.addEventListener('click', () => {
-		const timerElement = document.querySelector('countdown-timer');
-		timerElement.dispatchEvent(new CustomEvent('pauseTimer'));
-	});
+// window.addEventListener('DOMContentLoaded', () => {
+// 	const startButton = document.createElement('button');
+// 	startButton.textContent = 'Start Timer';
 
-	const resetButton = document.createElement('button');
-	resetButton.textContent = 'Reset Timer';
+// 	startButton.addEventListener('click', () => {
+// 		const timerElement = document.querySelector('countdown-timer');
+// 		timerElement.dispatchEvent(new CustomEvent('startTimer'));
+// 	});
 
-	resetButton.addEventListener('click', () => {
-		const timerElement = document.querySelector('countdown-timer');
-		timerElement.dispatchEvent(new CustomEvent('resetTimer'));
-	});
+// 	const pauseButton = document.createElement('button');
+// 	pauseButton.textContent = 'Pause Timer';
 
-//     // Добавляем кнопки в DOM
-	document.body.appendChild(startButton);
-	document.body.appendChild(pauseButton);
-	document.body.appendChild(resetButton);
-});
+// 	pauseButton.addEventListener('click', () => {
+// 		const timerElement = document.querySelector('countdown-timer');
+// 		timerElement.dispatchEvent(new CustomEvent('pauseTimer'));
+// 	});
+
+// 	const resetButton = document.createElement('button');
+// 	resetButton.textContent = 'Reset Timer';
+
+// 	resetButton.addEventListener('click', () => {
+// 		const timerElement = document.querySelector('countdown-timer');
+// 		timerElement.dispatchEvent(new CustomEvent('resetTimer'));
+// 	});
+
+// //     // Добавляем кнопки в DOM
+// 	document.body.appendChild(startButton);
+// 	document.body.appendChild(pauseButton);
+// 	document.body.appendChild(resetButton);
+// });
